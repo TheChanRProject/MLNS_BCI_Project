8a3bdb1145500a8360edebf5b4bc25460dc6986c
Rishov Chatterjee <garpchoo@gmail.com>
2019-04-27 21:09:03 -0700

    Editing

--- src/preprocessing/merging.py
+++ src/preprocessing/merging.py
@@ -3,4 +3,8 @@ from os import listdir
 
 def find_csv_filenames( path_to_dir, suffix=".csv" ):
     filenames = listdir(path_to_dir)
-    return [ filename for filename in filenames if filename.endswith( suffix ) ] 
+    return [ filename for filename in filenames if filename.endswith( suffix ) ]
+
+devY = find_csv_filenames('data/DevAttY', suffix=".csv")
+
+df = pd.concat([pd.read_csv(f"data/DevAttY/{i}") for i in devY])
