62aaca8081c85d25242865f8b35c4d5d3a8fb226
Rishov Chatterjee <garpchoo@gmail.com>
2019-04-27 22:46:01 -0700

    Editing

--- src/preprocessing/merging.py
+++ src/preprocessing/merging.py
@@ -5,10 +5,12 @@ def find_csv_filenames( path_to_dir, suffix=".csv" ):
     filenames = listdir(path_to_dir)
     return [ filename for filename in filenames if filename.endswith( suffix ) ]
 
-devY = find_csv_filenames('data/DevAttY', suffix=".csv")
+devX = find_csv_filenames('data/DevAttX', suffix=".csv")
 
-df = pd.concat([pd.read_csv(f"data/DevAttY/{i}") for i in devY])
+df = pd.concat([pd.read_csv(f"data/DevAttX/{i}") for i in devX])
 
 print(len(df))
 
-df.to_csv("data/Merged/merged_DevAttentionY.csv")
+df.head()
+
+df.to_csv("data/Merged/merged_DevAttentionX.csv")
