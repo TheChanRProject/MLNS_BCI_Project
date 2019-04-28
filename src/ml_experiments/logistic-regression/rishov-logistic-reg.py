d8fc952449b812833bf8d78cfaf1e09c40b72f7a
Rishov Chatterjee <garpchoo@gmail.com>
2019-04-28 04:11:16 -0700

    Editing

--- src/ml_experiments/logistic-regression/rishov-logistic-reg.py
+++ src/ml_experiments/logistic-regression/rishov-logistic-reg.py
@@ -7,4 +7,9 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from scikitplot.metrics import plot_roc, plot_confusion_matrix
 from warnings import filterwarnings
-filterwarnings("ignore") 
+filterwarnings("ignore")
+
+NeurFeats = pd.read_csv("data/Merged/merged_DevAttentionX.csv")
+NeurFeats.drop(list(NeurFeats.columns)[0], axis=1, inplace=True)
+NeurFeats.rename(columns={j:f"Feature_{i+1}" for i,j in enumerate(NeurFeats)}, inplace=True)
+NeurFeats.head()
