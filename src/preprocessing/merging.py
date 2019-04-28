07c9ec8f83104d9e38cf89f26e17f7a5f0cd22d6
Rishov Chatterjee <garpchoo@gmail.com>
2019-04-28 12:51:20 -0700

    Editing

--- src/preprocessing/merging.py
+++ src/preprocessing/merging.py
@@ -9,11 +9,4 @@ devX = find_csv_filenames('data/DevAttX', suffix=".csv")
 
 df = pd.concat([pd.read_csv(f"data/DevAttX/{i}") for i in devX])
 
-print(len(df))
-
-print(df.shape)
-column_dict = {j:f"Feature_{i+1}" for i,j in enumerate(df)}
-print(column_dict)
-df.rename(columns=column_dict)
-
 df.to_csv("data/Merged/merged_DevAttentionX.csv")
