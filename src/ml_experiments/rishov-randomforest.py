574b6fbe3289a1a50bd6f01216f8e8aa82e2c8e1
Rishov Chatterjee <garpchoo@gmail.com>
2019-04-28 00:30:21 -0700

    Editing

---
+++ src/ml_experiments/rishov-randomforest.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scikitplot.metrics import plot_roc, plot_confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import scale
+from sklearn.metrics import accuracy_score
+from warnings import filterwarnings
+filterwarnings("ignore")
+
+# X File Read in Pandas
+NeurFeat = pd.read_csv("../data/Merged/merged_DevAttentionX.csv")
+NeurFeat.drop(list(NeurFeat.columns)[0], axis=1, inplace=True)
+print(NeurFeat.shape)
+
+# Y File Read in Pandas
+ClassValues = pd.read_csv("../data/Merged/merged_DevAttentionY.csv")
+ClassValues.drop(list(ClassValues.columns)[0], axis=1, inplace=True)
+print(ClassValues.shape)
+
+NeurFeatExp = NeurFeat.loc[0:119999]
+ClassValuesExp = ClassValues.loc[0:119999]
+
+ZFeatures = scale(NeurFeatExp, axis=0)
+# Creating Training and Testing Sets
+Xtrain, Xtest, Ytrain, Ytest = train_test_split(ZFeatures, ClassValuesExp, test_size=0.2, random_state=100)
+
+# Create the validation sets
+Xtrain2, XVal, Ytrain2, YVal = train_test_split(Xtrain, Ytrain, test_size=0.2, random_state=100)
+
+model = RandomForestClassifier(n_estimators=1000, criterion="entropy")
+model_fit = model.fit(Xtrain2, Ytrain2)
+test_pred = model_fit.predict(Xtest)
+print(accuracy_score(Ytest, test_pred)) 
