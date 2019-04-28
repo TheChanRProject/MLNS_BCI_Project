cdfe1a3f251d3a5033d3e047c004a691cf236b24
Rishov Chatterjee <garpchoo@gmail.com>
2019-04-28 12:51:01 -0700

    Editing

---
+++ src/preprocessing/feature-labels.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from os import listdir
+
+def find_csv_filenames( path_to_dir, suffix=".csv" ):
+    filenames = listdir(path_to_dir)
+    return [ filename for filename in filenames if filename.endswith( suffix ) ]
+
+devX = find_csv_filenames('data/DevAttX', suffix=".csv")
+
+df = pd.concat([pd.read_csv(f"data/DevAttX/{i}") for i in devX])
+
+print(len(df))
+
+print(df.shape)
+column_dict = {j:f"Feature_{i+1}" for i,j in enumerate(df)}
+print(column_dict)
+df.rename(columns=column_dict)
+
+df.to_csv("data/Merged/merged_DevAttentionX.csv")
