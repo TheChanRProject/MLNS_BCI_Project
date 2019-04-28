9715f8fa3c9794c0a486ec9f4b9e14d834ec400e
Rishov Chatterjee <garpchoo@gmail.com>
2019-04-28 04:29:57 -0700

    Editing

--- src/ml_experiments/logistic-regression/rishov-logistic-reg.py
+++ src/ml_experiments/logistic-regression/rishov-logistic-reg.py
@@ -30,3 +30,18 @@ ClassValuesExp = ClassValues.loc[0:119999]
 ZNeurFeat = scale(NeurFeatExp, axis=0)
 
 # Splitting into Training and Testing Sets
+Xtrain, Xtest, Ytrain, Ytest = train_test_split(ZNeurFeat, ClassValuesExp, test_size=0.2, random_state=100)
+
+# Splitting into Training and Validation Sets
+Xtrain2, Xval, Ytrain2, Yval = train_test_split(Xtrain, Ytrain, test_size=0.2, random_state=100)
+
+# Building a Logistic Regression Model
+
+model = LogisticRegression()
+model_fit = model.fit(Xtrain2, Ytrain2)
+test_pred = model_fit.predict(Xtest)
+test_pred_prob = model_fit.predict_proba(Xtest)
+
+# Accuracy Score and Plotting ROC Curve and Confusion Matrix
+print(f"Accuracy of model is: {round(accuracy_score(Ytest, test_pred)*100, 2)}%")
+plot_roc()
