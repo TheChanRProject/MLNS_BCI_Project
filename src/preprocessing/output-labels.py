9fee174dcf0b971a478be28452be65189e330ffc
Rishov Chatterjee <garpchoo@gmail.com>
2019-04-28 20:25:25 -0700

    Editing

---
+++ src/preprocessing/output-labels.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from os import listdir
+
+def find_csv_filenames( path_to_dir, suffix=".csv" ):
+    filenames = listdir(path_to_dir)
+    return [ filename for filename in filenames if filename.endswith( suffix ) ]
+
+devY = find_csv_filenames('data/DevAttY', suffix=".csv")
+
+df = pd.concat([pd.read_csv(f"data/DevAttY/{i}") for i in devY])
+
+print(len(df))
+
+print(df.shape)
+column_dict = {j:f"Feature_{i+1}" for i,j in enumerate(df)}
+print(column_dict)
+df.rename(columns=column_dict, inplace=True)
+
+df.to_csv("data/Merged/merged_labeled_DevAttentionX.csv")
