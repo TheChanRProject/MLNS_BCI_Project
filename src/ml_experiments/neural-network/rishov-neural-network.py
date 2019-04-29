1c646828dc12cb6b3b4846b460b98bc61d63741d
Rishov Chatterjee <garpchoo@gmail.com>
2019-04-28 22:08:10 -0700

    Editing

--- src/ml_experiments/neural-network/rishov-neural-network.py
+++ src/ml_experiments/neural-network/rishov-neural-network.py
@@ -6,3 +6,5 @@ from sklearn.preprocessing import scale
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 from scikitplot.metrics import plot_roc, plot_confusion_matrix
+from warnings import filterwarnings
+filterwarnings("ignore")
