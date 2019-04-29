bb942ce844b6074d1f77516c6bb59ed483c14e32
Rishov Chatterjee <garpchoo@gmail.com>
2019-04-28 23:44:27 -0700

    Editing

--- src/ml_experiments/logistic-regression/logistic-sgd.py
+++ src/ml_experiments/logistic-regression/logistic-sgd.py
@@ -1,1 +1,11 @@
-# This is the code for implementing a logistic regression with stochastic gradient descent 
+# This is the code for implementing a logistic regression with stochastic gradient descent
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import scale
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import SGDClassifier
+from scikitplot.metrics import plot_roc, plot_confusion_matrix
+from warnings import filterwarnings
+filterwarnings("ignore")
