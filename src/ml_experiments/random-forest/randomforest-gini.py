9c0121f21c5b9c09c8f3a3a702db421f3d58aa41
Rishov Chatterjee <garpchoo@gmail.com>
2019-04-29 02:50:05 -0700

    Editing

--- src/ml_experiments/random-forest/randomforest-gini.py
+++ src/ml_experiments/random-forest/randomforest-gini.py
@@ -10,3 +10,14 @@ from warnings import filterwarnings
 filterwarnings("ignore")
 from jupyterthemes import jtplot
 jtplot.style(theme='monokai')
+
+NeurFeat = pd.read_csv("data/Merged/merged_labeled_DevAttentionX.csv").loc[0:119999]
+ClassValues = pd.read_csv("data/Merged/merged_labeled_DevAttentionY.csv").loc[0:119999]
+NeurFeat.drop(list(NeurFeat.columns)[0], axis=1, inplace=True)
+ClassValues.drop(list(ClassValues.columns)[0], axis=1, inplace=True)
+zFeat = scale(NeurFeat, axis=0)
+
+# Training and Testing Sets
+Xtrain1, Xtest, Ytrain1, Ytest = train_test_split(zFeat, ClassValues, test_size=0.2, random_state=100)
+# Training -> Training and Validation Sets
+Xtrain2, Xval, Ytrain2, Yval = train_test_split(Xtrain1, Ytrain1, test_size=0.2, random_state=100)
