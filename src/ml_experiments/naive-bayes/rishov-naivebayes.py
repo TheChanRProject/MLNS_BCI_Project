5c7fac9df938e09ffdf2a3f620bf6dcf554f5970
Rishov Chatterjee <garpchoo@gmail.com>
2019-04-28 21:06:17 -0700

    Editing

--- src/ml_experiments/naive-bayes/rishov-naivebayes.py
+++ src/ml_experiments/naive-bayes/rishov-naivebayes.py
@@ -9,13 +9,12 @@ from scikitplot.metrics import plot_roc, plot_confusion_matrix
 from warnings import filterwarnings
 filterwarnings("ignore")
 
-# Reading in Features from Labeled DevAttentionX Data
-neuralFeatures = pd.read_csv("data/Merged/merged_labeled_DevAttentionX.csv")
-# Reading in Outputs: Attended or Unattended from Labeled DevAttentionY Data 
+# Reading in Features from Labeled DevAttentionX Data with first 120000 rows
+neuralFeatures = pd.read_csv("data/Merged/merged_labeled_DevAttentionX.csv").loc[0:119999]
+# Drop Unnamed column
+neuralFeatures.drop(list(neuralFeatures.columns)[0], axis=1, inplace=True)
+# Reading in Outputs: Attended or Unattended from Labeled DevAttentionY Data
 classValues = pd.read_csv("data/Merged/merged_labeled_DevAttentionY.csv")
-
-# Scaling neuralFeatures
-zFeatures = scale(neuralFeatures, axis=0)
 
 # Creating Training and Testing Sets
 Xtrain, Xtest, Ytrain, Ytest = train_test_split(zFeatures, classValues, test_size=0.2, random_state=100)
