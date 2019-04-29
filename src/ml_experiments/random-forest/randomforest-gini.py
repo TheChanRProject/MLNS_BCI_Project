6a39b9a6e0e86d9f2870322f1649053a9850862d
Rishov Chatterjee <garpchoo@gmail.com>
2019-04-29 02:44:28 -0700

    Editing

---
+++ src/ml_experiments/random-forest/randomforest-gini.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scikitplot.metrics import plot_roc, plot_confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import scale
+from sklearn.metrics import accuracy_score
+from warnings import filterwarnings
+filterwarnings("ignore")
