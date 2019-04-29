732b4344793745681e3ba445e00052883e80af13
Rishov Chatterjee <garpchoo@gmail.com>
2019-04-28 22:07:22 -0700

    Editing

---
+++ src/ml_experiments/neural-network/rishov-neural-network.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import scale
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+from scikitplot.metrics import plot_roc, plot_confusion_matrix
