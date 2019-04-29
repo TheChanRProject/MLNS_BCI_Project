3a54e4b53b8557c1a64a1c9c8af24085ccd7e04c
Rishov Chatterjee <garpchoo@gmail.com>
2019-04-29 01:41:54 -0700

    Editing

--- src/ml_experiments/logistic-regression/logistic-sgd.py
+++ src/ml_experiments/logistic-regression/logistic-sgd.py
@@ -55,3 +55,4 @@ log_l1_prob = log_l1_fit.predict_proba(Xtest)
 
 # Plot ROC and Confusion Matrix
 plot_roc(Ytest, log_l1_prob, title="Logistic Regression with SGD and L1 ROC")
+plot_confusion_matrix(Ytest, log_l1_pred, title="Logistic Regression with SGD and L1 Confusion Matrix")
