e0087832595a0b7a569854d4801902fdd2047f29
Rishov Chatterjee <garpchoo@gmail.com>
2019-04-28 20:54:25 -0700

    Editing

--- src/ml_experiments/naive-bayes/rishov-naivebayes.py
+++ src/ml_experiments/naive-bayes/rishov-naivebayes.py
@@ -13,3 +13,14 @@ filterwarnings("ignore")
 neuralFeatures = pd.read_csv("data/Merged/merged_labeled_DevAttentionX.csv").loc[0:119999]
 # Reading in Outputs: Attended or Unattended from Labeled DevAttentionY Data with first 120000
 classValues = pd.read_csv("data/Merged/merged_labeled_DevAttentionY.csv").loc[0:119999]
+
+# Scaling neuralFeatures
+zFeatures = scale(neuralFeatures, axis=0)
+
+# Creating Training and Testing Sets
+Xtrain, Xtest, Ytrain, Ytest = train_test_split(zFeatures, classValues, test_size=0.2, random_state=100)
+
+# Partitioning Training Set into Training and Validation Sets
+Xtrain2, Xval, Ytrain2, Yval = train_test_split(Xtrain, Ytrain, test_size=0.2, random_state=100)
+
+# Building a Naive Bayes Classifier Assuming a Gaussian Distribution 
