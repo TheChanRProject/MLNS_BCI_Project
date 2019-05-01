031fb1cc1ecf0997b3bfb3f3031d6f0d0f7211de
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-01 16:35:24 -0700

    Editing

--- src/cross-val/randomized-search.py
+++ src/cross-val/randomized-search.py
@@ -5,4 +5,10 @@ from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import scale
+from sklearn.metrics import accuracy_score
 from scikitplot.metrics import plot_roc, plot_confusion_matrix
+from jupyterthemes import jtplot
+jtplot.style(theme="monokai")
+from sklearn.tree import export_graphviz
+import pydot
