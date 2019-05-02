8afe0b4cafaffb69a75137a53a22732b0adfed86
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-02 02:28:20 -0700

    Editing

--- src/cross-val/grid-search.py
+++ src/cross-val/grid-search.py
@@ -17,11 +17,10 @@ def RandomForest(x_df, y_df, cv, n):
               "criterion": ["gini", "entropy"]}
     grid_search = GridSearchCV(model, param_distributions=params, cv=cv, n_iter=n)
     grid_fit = grid_search.fit(x_df, y_df)
-    cv_scores = cross_val_score(model, x_df, y_df, cv=cv)
-    print(cv_scores)
-    print(f"Mean accuracy: {round(cv_scores.mean()*100,2)}%")
-    print(f"Standard Deviation of Accuracies: {round(cv_scores.std()*100,2)}%")
-    return random_fit
+    print(random_fit.cv_results_)
+    print(f"Best Cross-Validated Accuracy: {round(grid_fit.best_score_*100,2)}%")
+    print(f"Best Model Parameters: {grid_fit.best_params_}")
+    return grid_fit
 
 # Cross Validation Function for Logistic Regression Non-SGD
 def noSGDLogisticRegression(x_df, y_df, cv, n):
