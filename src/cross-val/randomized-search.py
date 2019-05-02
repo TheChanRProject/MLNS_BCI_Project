80d5d807b44b51cdec0a1fd3da06a30f74e79a09
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-01 17:01:38 -0700

    Editing

--- src/cross-val/randomized-search.py
+++ src/cross-val/randomized-search.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.model_selection import cross_val_score, train_test_split, RandomizedSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.neural_network import MLPClassifier
@@ -12,3 +12,19 @@ from jupyterthemes import jtplot
 jtplot.style(theme="monokai")
 from sklearn.tree import export_graphviz
 import pydot
+
+# Function Approach for Our Data
+
+neurFeat = pd.read_csv("data/Merged/merged_labeled_DevAttentionX.csv")
+neurFeat.drop(list(neurFeat.columns)[0], axis=1, inplace=True)
+classValues = pd.read_csv("data/Merged/merged_labeled_DevAttentionY.csv")
+classValues.drop(list(classValues.columns)[0], axis=1, inplace=True)
+
+# Cross Validation Function for Random Forest
+def RandomForest(x_df, y_df, cv, n_estimators, criterion, bootstrap, random_state):
+    NeurFeat = x_df
+    classValues = y_df
+    zFeat = scale(NeurFeat)
+    X_train, X_test, Y_train, Y_test = train_test_split(zFeat, classValues, test_size=0.0, random_state=100)
+    model = RandomForestClassifier(n_estimators=n_estimators, criterion=criterion, bootstrap=bootstrap, random_state=random_state)
+    model_fit = model.fit()
