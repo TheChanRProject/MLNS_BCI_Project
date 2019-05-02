f8d788ca38a49108894fdbce2d2eaccb5ee7fe55
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-01 23:42:45 -0700

    Editing

--- src/cross-val/grid-search.py
+++ src/cross-val/grid-search.py
@@ -35,3 +35,20 @@ def noSGDLogisticRegression(x_df, y_df, cv, n):
     print(f"Mean accuracy: {round(cv_scores.mean()*100,2)}%")
     print(f"Standard Deviation of Accuracies: {round(cv_scores.std()*100,2)}%")
     return grid_fit
+
+# Cross Validation Function for Logistic Regression SGD
+
+def SGDLogisticRegression(x_df, y_df, cv, n):
+    model = SGDClassifier(loss='log', random_state=100, verbose=True)
+    params = {"penalty": ['l1', 'l2', 'elasticnet'],
+              "l1_ratio": [0.15, 0.25],
+              "shuffle": [True, False],
+              "learning_rate": ['constant', 'adaptive']
+              }
+    grid_search = GridSearchCV(model, param_distributions=params, cv=cv, n_iter=n)
+    grid_fit = grid_search.fit(x_df, y_df)
+    cv_scores = cross_val_score(model, x_df, y_df, cv=cv)
+    print(cv_scores)
+    print(f"Mean accuracy: {round(cv_scores.mean()*100,2)}%")
+    print(f"Standard Deviation of Accuracies: {round(cv_scores.std()*100,2)}%")
+    return grid_fit
