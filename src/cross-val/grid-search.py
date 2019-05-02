53e357a44370235e3f02d403e991605da51f9e52
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-02 01:55:20 -0700

    Editing

--- src/cross-val/grid-search.py
+++ src/cross-val/grid-search.py
@@ -6,7 +6,6 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import scale
-from sklearn.metrics import accuracy_score
 from warnings import filterwarnings
 filterwarnings("ignore")
 
