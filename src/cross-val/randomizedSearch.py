d7437f094afcba2d662c6d7fa068b8fb93864c18
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-02 00:15:40 -0700

    Editing

--- src/cross-val/randomizedSearch.py
+++ src/cross-val/randomizedSearch.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import cross_val_score, RandomizedSearchCV
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.neural_network import MLPClassifier
