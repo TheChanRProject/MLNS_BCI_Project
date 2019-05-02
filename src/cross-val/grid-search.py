c9ff8e9c3eff0ea0aab8aacc4a2f930a3034a2c4
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-01 23:37:26 -0700

    Editing

--- src/cross-val/grid-search.py
+++ src/cross-val/grid-search.py
@@ -9,3 +9,17 @@ from sklearn.preprocessing import scale
 from sklearn.metrics import accuracy_score
 from warnings import filterwarnings
 filterwarnings("ignore")
+
+# Cross Validation Function for Random Forest
+def RandomForest(x_df, y_df, cv, n):
+    model = RandomForestClassifier(random_state=100, verbose=True)
+    params = {"n_estimators": [400, 1000],
+              "bootstrap": [True, False],
+              "criterion": ["gini", "entropy"]}
+    grid_search = GridSearchCV(model, param_distributions=params, cv=cv, n_iter=n)
+    grid_fit = grid_search.fit(x_df, y_df)
+    cv_scores = cross_val_score(model, x_df, y_df, cv=cv)
+    print(cv_scores)
+    print(f"Mean accuracy: {round(cv_scores.mean()*100,2)}%")
+    print(f"Standard Deviation of Accuracies: {round(cv_scores.std()*100,2)}%")
+    return random_fit
