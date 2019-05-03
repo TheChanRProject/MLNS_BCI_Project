51d46fb1ece33c6a5d9bf250959ff5db222268f6
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-03 14:12:25 -0700

    Editing

--- src/cross-val/roc.py
+++ src/cross-val/roc.py
@@ -1,9 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import roc_curve, auc
 
 
 # Test with function from randomized search
-def cv_roc(n, fit_function, Ytest, Xtest):    
+def cv_roc(n, fit_function, Ytest, Xtest):
     probas = fit_function.predict_proba(Xtest)
