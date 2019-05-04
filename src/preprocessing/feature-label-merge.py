0828e6d73b0ed8843c8ba76e0472efad5a3364b6
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-04 15:54:44 -0700

    Editing

--- src/preprocessing/feature-label-merge.py
+++ src/preprocessing/feature-label-merge.py
@@ -19,5 +19,11 @@ VPgeo_XFrame = pd.read_csv(f"data/DevAttX/{devX[0]}")
 VPgeo_YFrame = pd.read_csv(f"data/DevAttY/{devY[0]}")
 column_dict = {j:f"Feature_{i+1}" for i,j in enumerate(VPgeo_XFrame)}
 print(column_dict)
+Y_map = {j:f"Label" for i,j in enumerate(VPgeo_YFrame)}
+VPgeo_YFrame.rename(columns=Y_map, inplace=True)
 VPgeo_XFrame.rename(columns=column_dict, inplace=True)
 print(VPgeo_XFrame.columns)
+print(VPgeo_YFrame.columns)
+VPgeo_XFrame['Label'] = VPgeo_YFrame['Label']
+VPgeo_XYFrame = VPgeo_XFrame
+print(len(VPgeo_XYFrame.columns))
