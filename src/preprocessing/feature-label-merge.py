2b712ff6e10556c155b95f4748c49d9bf1e7c0f6
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-04 15:28:28 -0700

    Editing

--- src/preprocessing/feature-label-merge.py
+++ src/preprocessing/feature-label-merge.py
@@ -1,2 +1,6 @@
 import pandas as pd
 from os import listdir
+
+def find_csv_filenames( path_to_dir, suffix=".csv" ):
+    filenames = listdir(path_to_dir)
+    return [ filename for filename in filenames if filename.endswith( suffix ) ]
