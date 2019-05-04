2f5dc49d8ae6f45888c28166c14f7132154085ca
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-04 15:18:44 -0700

    Editing

---
+++ src/preprocessing/random-merge.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from os import listdir
+
+def find_csv_filenames( path_to_dir, suffix=".csv" ):
+    filenames = listdir(path_to_dir)
+    return [ filename for filename in filenames if filename.endswith( suffix ) ]
+
+devX = find_csv_filenames('data/DevAttX', suffix=".csv")
+
+df = pd.concat([pd.read_csv(f"data/DevAttX/{i}") for i in devX])
+
+df.to_csv("data/Merged/merged_DevAttentionX.csv")
