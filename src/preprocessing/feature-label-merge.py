bbfffff67326fcd934e85072a1e6dd705a19c6bf
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-04 16:07:02 -0700

    Editing

--- src/preprocessing/feature-label-merge.py
+++ src/preprocessing/feature-label-merge.py
@@ -44,4 +44,18 @@ VPaan_XYFrame = VPaan_XFrame
 print(len(VPaan_XYFrame.columns))
 VPaan_XYFrame.to_csv("data/DevAttXY/VPaan.csv")
 
-#
+print(devX)
+# VPaak Data Frame 3
+VPaak_XFrame = pd.read_csv(f"data/DevAttX/{devX[2]}")
+VPaak_YFrame = pd.read_csv(f"data/DevAttY/{devY[2]}")
+column_dict = {j:f"Feature_{i+1}" for i,j in enumerate(VPaak_XFrame)}
+print(column_dict)
+Y_map = {j:f"Label" for i,j in enumerate(VPaak_YFrame)}
+VPaak_YFrame.rename(columns=Y_map, inplace=True)
+VPaak_XFrame.rename(columns=column_dict, inplace=True)
+print(VPaak_XFrame.columns)
+print(VPaak_YFrame.columns)
+VPaak_XFrame['Label'] = VPaak_YFrame['Label']
+VPaak_XYFrame = VPaak_XFrame
+print(len(VPaak_XYFrame.columns))
+VPaak_XYFrame.to_csv("data/DevAttXY/VPaak.csv")
