49282323a9cde419bdcea1e460526e4c78e2bb26
Rishov Chatterjee <garpchoo@gmail.com>
2019-05-07 22:12:50 -0700

    Editing

--- src/ml_experiments/neural-network/visualization.py
+++ src/ml_experiments/neural-network/visualization.py
@@ -8,5 +8,5 @@ from ann_visualizer.visualize import ann_viz
 network = Sequential()
 
 network.add(Dense(units=100,activation='relu',kernel_initializer='uniform',input_dim=189))
-network.add(Dense(units=2, activation='relu', kernel_initializer='uniform'))
+network.add(Dense(units=1, activation='sigmoid', kernel_initializer='uniform'))
 ann_viz(network, title="Our Neural Network")
